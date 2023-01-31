13c940f89383a584d36fa052444a170bc308423e
iskren-kolev <iskren.k95@gmail.com>
2023-01-31 19:02:29 +0200

    First version of website1

---
+++ javascript/hamburger_menu.js
@@ -0,0 +1,10 @@
+
+//to open the hamburger menu
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
