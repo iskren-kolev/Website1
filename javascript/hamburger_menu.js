c5fddd56e22e839d7e6735d24d1833685a3bee45
iskren-kolev <iskren.k95@gmail.com>
2023-02-13 13:34:57 +0200

    added JS selector and rename the JS function

--- javascript/hamburger_menu.js
+++ javascript/hamburger_menu.js
@@ -1,6 +1,10 @@
 
+//JS selector with added event listener to open a function
+let hamburger_button = document.querySelector('.icon');
+hamburger_button.addEventListener('click', open_hamburger_menu);
+
 //to open the hamburger menu
-function myFunction() {
+function open_hamburger_menu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
