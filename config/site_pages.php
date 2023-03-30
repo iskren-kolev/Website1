6c215eba1bf794df51a46dbe719619c028fe69dd
Iskren <you@example.com>
2023-03-30 10:15:56 +0300

    Change_header_name.php renamed to site_pages.php. Ligics code from site_pages.php moved to modules/header_module.php

---
+++ config/site_pages.php
@@ -0,0 +1,8 @@
+<?php
+// Define an array of page titles and their corresponding URLs
+$pages = array(
+    'CV' => '/index.php',
+    'About me' => '/aboutme.php',
+    'Contact' => '/contacts.php'
+);
+?>
--- modules/header_module.php
+++ modules/header_module.php
@@ -1,5 +1,25 @@
 <header>
     <h1>Iskren Kolev</h1>
-    <?php include "./config/change_header_name.php" ?>
     <p id="under_name"></p>
 </header>
+
+<?php
+    include "./config/site_pages.php";
+
+    // Get the current URL path
+    $pathName = $_SERVER['REQUEST_URI'];
+
+    // Find the corresponding page title for the current URL path
+    $pageTitle = 'CV'; // Default to 'CV'
+
+    foreach ($pages as $title => $url) {
+        if ($url === $pathName) {
+            $pageTitle = $title;
+        break;
+        }
+    }
+
+    // Output the page title
+    echo '<span id="under_name">' . $pageTitle . '</span>';
+    ?>
+
