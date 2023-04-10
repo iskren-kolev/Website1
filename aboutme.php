2a6c0e215cf1ea51c17cf0bb95ba7ace467ed339
Iskren <you@example.com>
2023-04-10 17:46:35 +0300

    created new JS file with the timer functionality

--- aboutme.php
+++ aboutme.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 	<head>
 		<title>Iso's site</title>		
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,24 +11,14 @@
 	</head>
 
 	<body>
-    <?php include ('./modules/header_module.php'); ?>
+        <?php include ('./modules/header_module.php'); ?>
+        <?php include ('./modules/navigation_bar_module.php') ?>
 
-    <?php include ('./modules/navigation_bar_module.php') ?>
-
-		<section>
-			<h2 id="summaryh2">About me</h2>
-			<p class="summary"><img src="./sources/pic_of_me.jpg" width="80px" alt="IMAGE">
-			Hello,<br>
-			I am Iskren from Ruse,Bulgaria. 
-			I am responsible, disciplined and creative. 
-			I like to create and fix things. In my free time, I like to ride a bike and a motorcycle. 
-			I like walks in the mountains and forests. 
-			My hobby is restoring old cars and motorbikes as well as repairing battery packs.</p>
-        </section>
+        <br>
+        <br>
 
         <?php include ('./modules/footer_module.php');?>
-
 		<script src="./javascript/hamburger_menu.js"></script>
-
+		<script src="./javascript/countdown.js"></script>
     </body>
 </html>
