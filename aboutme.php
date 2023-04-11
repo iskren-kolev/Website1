7679abe46a82ab80f4ab6d82c478078d6f904aec
Iskren <you@example.com>
2023-04-11 14:15:17 +0300

    visual and functional changes made

--- aboutme.php
+++ aboutme.php
@@ -13,9 +13,11 @@
 	<body>
         <?php include ('./modules/header_module.php'); ?>
         <?php include ('./modules/navigation_bar_module.php') ?>
-        <section id="timer_section">
+        <section>
             <h2 id="summaryh2">Timer</h2>
-
+            <div id="timerSection"></div>
+            <img id="timerImage" src="./sources/timer.png">
+            <button id="resetButton">Reset</button>
         </section>
         <?php include ('./modules/footer_module.php');?>
 		<script src="./javascript/hamburger_menu.js"></script>
--- javascript/countdown.js
+++ javascript/countdown.js
@@ -1,43 +1,28 @@
-let remainingTime = 5;
-let displayElement = document.createElement("div");
-let timerSection = document.getElementById("timer_section");
-
-let displayButton = document.createElement("button");
-displayButton.textContent="Reset";
-displayButton.id="resetButton";
-let displayPicture=document.createElement("img");
-displayPicture.src=src="./sources/pic_of_me.jpg";
-
-function showTimer(){
-    if (!timerSection.contains(displayElement)) {
-        timerSection.appendChild(displayElement);
-    }
-}
-showTimer();
+let remainingTime = 8;
+let timerSection = document.getElementById("timerSection");
+let timerImage = document.getElementById("timerImage");
+let resetButton = document.getElementById("resetButton")
 
 function updateDisplay(){
     let mins = Math.floor(remainingTime/60);
     let secs = remainingTime%60;
-    displayElement.textContent=(`${mins} mins ${secs} secs`);
+    timerSection.innerHTML=(`${mins} mins ${secs} secs`);
 }
 function updateTimer(){
-    showTimer();
     remainingTime--;
     updateDisplay();
     if (remainingTime===0){
-        displayElement.remove();
-        timerSection.appendChild(displayPicture);
-        timerSection.appendChild(displayButton);
         clearInterval(intervalID);
+        timerImage.style.display="flex";
     }
 }
 function buttonClicked(){
-    remainingTime = 5;
-    displayButton.remove();
-    displayPicture.remove();
+    timerImage.style.display="none";
+    clearInterval(intervalID);
+    remainingTime = 8;
     intervalID = setInterval(updateTimer, 1000);
 }
-displayButton.addEventListener("click", buttonClicked);
+resetButton.addEventListener("click", buttonClicked);
 
 let intervalID = setInterval(updateTimer, 1000);
 
