7ebb6dda031c0ae3b967c5f45781dbabc2fbb944
Iskren <you@example.com>
2023-04-21 13:53:35 +0300

    More options to img slider

--- javascript/image_slider.js
+++ javascript/image_slider.js
@@ -5,98 +5,98 @@ class ImageSliderBox {
         this.imageSliderBox = imageSliderBox;
         this.img_length = this.imageSliderBox.getElementsByTagName("img").length;
         this.num = 0;
-        this.roll = false;
         this.prevButton = this.imageSliderBox.querySelector('.prev');
         this.nextButton = this.imageSliderBox.querySelector('.next');
         this.prevButton.addEventListener("click", () => this.prev());
-        this.nextButton.addEventListener("click", () => this.next(this.roll));
-        this.dots = this.imageSliderBox.querySelector(".dots")
-        this.dataTypeDots = this.imageSliderBox.getAttribute("data-slider-options");
-        this.dataTypeRoll = this.imageSliderBox.getAttribute("data-roll-options");
-        this.dataStartSlide = this.imageSliderBox.getAttribute("data-start-from");
-        this.setOptions();
+        this.nextButton.addEventListener("click", () => this.next());
+        this.dots = this.imageSliderBox.querySelector(".dots");
+        this.slider_options = this.imageSliderBox.getAttribute("data-slider-options");
+        this.applyOptions();
+    }
+    hasOption(name) {
+        return !!this.slider_options.includes(name);
     }
 
-    setOptions() {
-        if (this.dataTypeDots === "nodots") {
+    applyOptions() {
+        if (this.slider_options.includes("nodots")) {
             this.dots.style.display = "none";
         }
-        if (this.dataTypeRoll === "infinate") {
-            this.roll = true;
+        if (this.slider_options.includes("start-from")) {
+            let expression = /start-from-(\d+)/;
+            let match = this.slider_options.match(expression);
+            let startFrom = match ? parseInt(match[1]) : null;
+            if(startFrom<=this.img_length) {
+                this.num = startFrom - 1;
+            }
         }
-        if (this.dataStartSlide) {
-            this.num = this.dataStartSlide-1;
-        } else {
-            this.num = 0;
-            //option for hiding prev button
-            this.prevButton.style.display = "none";
-        }
-        this.img = this.imageSliderBox.getElementsByTagName("img")[this.num];
-        this.dot = this.imageSliderBox.getElementsByTagName("span")[this.num];
-        this.img.style.display = "inline";
-        this.dot.setAttribute("id", "active");
+        this.changePicAndDot();
     }
 
-
-    change_pic_and_dot() {
-        this.dot.removeAttribute("id");
+    changePicAndDot() {
         this.img = this.imageSliderBox.getElementsByTagName("img")[this.num];
         this.dot = this.imageSliderBox.getElementsByTagName("span")[this.num];
+        this.textDescription = this.imageSliderBox.getElementsByTagName("p")[this.num];
+
         this.img.style.display = "inline";
         this.dot.setAttribute("id", "active");
+        this.textDescription.style.display = "inline";
         clearInterval(this.callEveryFiveSeconds);
-        this.callEveryFiveSeconds = setInterval(() => this.next(this.roll), 5000);
+        this.callEveryFiveSeconds = setInterval(() => this.next(), 5000);
     }
 
-    hide_show_arrows() {
-        //hide and show prev button
-        if (this.num !== 0) {
-            //display the prev Buttton
-            this.prevButton.style.display = "inline";
-        } else {
-            //hide the prev Buttton
+    hideShowArrows() {
+        if (this.num === 0) {
+            //hide the prev button
             this.prevButton.style.display = "none";
-        }
-        //hide and show next button
-        if (this.num === this.img_length - 1) {
+        } else if (this.num === this.img_length - 1) {
+            //hide the next button
             this.nextButton.style.display = "none";
         } else {
+            //display the prev and next buttons
+            this.prevButton.style.display = "inline";
             this.nextButton.style.display = "inline";
         }
     }
 
-    next(roll) {
-        if (roll) {
+    next() {
+        if (this.hasOption("infinate")) {
             this.num++;
             if (this.num < this.img_length) {
                 this.img.style.display = "none";
-                this.change_pic_and_dot();
+                this.textDescription.style.display = "none";
+                this.dot.removeAttribute("id");
+                this.changePicAndDot();
             } else {
                 this.num = 0;
                 this.img.style.display = "none";
-                this.change_pic_and_dot();
+                this.textDescription.style.display = "none";
+                this.dot.removeAttribute("id");
+                this.changePicAndDot();
             }
-        }
-        if (!roll) {
+        } else {
             if (this.num < this.img_length - 1) {
                 this.img.style.display = "none";
+                this.textDescription.style.display = "none";
                 this.num++;
-                this.change_pic_and_dot();
+                this.dot.removeAttribute("id");
+                this.changePicAndDot();
             }
         }
-        this.hide_show_arrows();
+        this.hideShowArrows();
     }
 
     prev() {
         if (this.num > 0) {
             this.img.style.display = "none";
+            this.textDescription.style.display = "none";
             this.num--;
-            this.change_pic_and_dot();
+            this.dot.removeAttribute("id");
+            this.changePicAndDot();
         }
-        this.hide_show_arrows();
+        this.hideShowArrows();
     }
 
-    callEveryFiveSeconds = setInterval(() => this.next(this.roll), 5000);
+    callEveryFiveSeconds = setInterval(() => this.next(), 5000);
 }
 
 sliders.forEach(slider => new ImageSliderBox(slider));
--- modules/gallery_module.php
+++ modules/gallery_module.php
@@ -1,13 +1,18 @@
 <div id="margin"></div>
 
-<div class="image_slider_box" data-start-from="2">
+<div class="image_slider_box" data-slider-options="start-from-2">
     <div class="img_slider">
         <button class="prev"><</button>
         <img src="sources/wallpaper1.jpg"/>
+        <p>изображение 1</p>
         <img src="sources/wallpaper2.jpg"/>
+        <p>изображение 2</p>
         <img src="sources/wallpaper3.jpg"/>
+        <p>изображение 3</p>
         <img src="sources/wallpaper4.jpg"/>
+        <p>изображение 4</p>
         <img src="sources/wallpaper5.jpg"/>
+        <p>изображение 5</p>
         <button class="next">></button>
     </div>
     <div class="dots">
@@ -19,12 +24,15 @@
     </div>
 </div>
 
-<div class="image_slider_box" data-slider-options="nodots" data-roll-options="infinate">
+<div class="image_slider_box" data-slider-options="nodots infinate">
     <div class="img_slider">
         <button class="prev"><</button>
         <img src="sources/wallpaper6.jpg"/>
+        <p>изображение 1</p>
         <img src="sources/wallpaper7.jpg"/>
+        <p>изображение 2</p>
         <img src="sources/wallpaper8.jpg"/>
+        <p>изображение 3</p>
         <button class="next">></button>
     </div>
     <div class="dots">
