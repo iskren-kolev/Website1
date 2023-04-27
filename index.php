11baf8c1e368926e957d14f30061da2c32cded55
Iskren <you@example.com>
2023-04-27 17:28:48 +0300

    Deleted unnecessary line

--- index.php
+++ index.php
@@ -79,7 +79,6 @@
 
 <script src="./javascript/hamburger_menu.js"></script>
 <script src="./javascript/image_slider.js"></script>
-<script src="./javascript/bg_video.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
