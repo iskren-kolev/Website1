232a37d25ec0629d254af637555279d1832f8c09
Iskren <you@example.com>
2023-04-28 10:12:52 +0300

    Added functionality to close the modal when click outside ot it and to have scroll option

--- modules/modal_module.php
+++ modules/modal_module.php
@@ -1,6 +1,43 @@
 <div id="myModal" class="modal">
     <div class="modal-content">
         <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
+        <h1>Tranquility by the Sea</h1>
+        <p>As I sat on the edge of the pier, my feet dangling in the cool water, I gazed out at the vast expanse of the
+            ocean before me. The sun was just beginning to set, casting a warm orange glow over the sky. Seagulls
+            swooped and circled overhead, their cries echoing in the salty air. I took a deep breath, inhaling the sea
+            breeze, and felt a sense of calm wash over me.
+            I had come to this quiet fishing village on the coast of Maine for a week of rest and relaxation. My job in
+            the city was stressful, and I needed to escape for a while. I had rented a small cabin on the outskirts of
+            town, and every day I walked down to the pier to watch the fishermen bring in their catch.
+            Today, however, I had decided to do something different. Instead of just watching, I wanted to experience
+            the sea for myself. I had rented a small kayak from a local shop, and I planned to paddle out into the open
+            water.
+            As I pushed off from the shore, the kayak bobbed and weaved beneath me. I had never been very comfortable in
+            the water, but I was determined to try. The sun beat down on my skin, and I felt the salt spray on my face.
+            It was exhilarating.
+            As I paddled farther out, the shore became a distant speck on the horizon. The ocean seemed infinite,
+            stretching out before me in all directions. It was simultaneously terrifying and awe-inspiring.
+            Suddenly, I felt a tug on my line. I had forgotten that I had brought my fishing gear with me. I reeled in
+            the line, excited to see what I had caught. It was a small fish, but it was a start.
+            Over the next hour, I caught several more fish. None of them were very big, but I felt a sense of
+            accomplishment with each one. I was no longer just an observer; I was a part of the sea.
+            As the sun began to set, I realized that I had gone farther out than I had intended. The shore was barely
+            visible in the distance. I started to paddle back, but the current was stronger than I had realized. I felt
+            myself being pulled away from the shore, towards the open ocean.
+            Panic set in. I had never felt so alone and vulnerable. The water was dark and cold, and I knew that I could
+            easily be swallowed up by the sea. But I refused to give up. I dug my paddle into the water, straining with
+            all my might.
+            And then I heard it. A soft humming, like a song. I turned my head, trying to locate the source of the
+            sound. And that's when I saw him.
+            A lone fisherman was sitting in a small boat, not far from me. He was humming to himself, as he patiently
+            reeled in his line. He looked so peaceful and content, surrounded by nothing but the ocean and the sky.
+            I paddled towards him, tears streaming down my face. He looked up and smiled. He could see the fear in my
+            eyes.
+            "Don't worry, son," he said. "I'll help you get back to shore."
+            He paddled over to me and tied a rope to the front of my kayak. Together, we slowly made our way back
+            towards the shore. He didn't say much, but his presence was enough. He was a rock in the midst of the storm.
+            When we finally reached the shore, I thanked him profusely. He just nodded and went back to his fishing.
+            Over the next few days, I saw him several more times. He was always out on the water, fishing. And every
+            time I saw him, I felt</p>
     </div>
 </div>
